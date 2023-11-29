498d96fd074d625054334f759010d8d88984b4b2
vickymaulana <vickymaulanna@gmail.com>
2023-11-29 10:40:55 +0700

    Struktur Update

--- lib/screens/data_view_screen.dart
+++ lib/screens/data_view_screen.dart
@@ -24,23 +24,89 @@ class _DataViewScreenState extends State<DataViewScreen> {
         builder: (BuildContext context, AsyncSnapshot<List<List<String>>> snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
           } else {
-            if (snapshot.hasError)
-              return Center(child: Text('Error: ${snapshot.error}'));
-            else
-              return ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  List<String> data = snapshot.data![index];
-                  return ListTile(
-                    title: Text('NIK: ${data[0]}, Nama: ${data[1]}'), // Displaying NIK and Nama
-                    subtitle: Text('Data Lengkap: ${data.join(', ')}'), // Displaying all data
-                  );
-                },
-              );
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                List<String> data = snapshot.data![index];
+                return Card(
+                  child: ListTile(
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Data Lengkap'),
+                            content: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                for (int i = 0; i < data.length; i++)
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      border: Border.all(color: Colors.grey),
+                                      borderRadius: BorderRadius.circular(8.0),
+                                    ),
+                                    padding: EdgeInsets.all(8.0),
+                                    margin: EdgeInsets.symmetric(vertical: 4.0),
+                                    child: Text(
+                                      '${_getFieldName(i)}: ${data[i]}',
+                                      style: TextStyle(fontSize: 16),
+                                    ),
+                                  ),
+                              ],
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text('Close'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    title: Text('NIK: ${data[0]}'),
+                    subtitle: Text('Nama: ${data[1]}'),
+                  ),
+                );
+              },
+            );
           }
         },
       ),
     );
+  }
+
+  String _getFieldName(int index) {
+    switch (index) {
+      case 0:
+        return 'NIK';
+      case 1:
+        return 'Nama';
+      case 2:
+        return 'Tanggal Lahir';
+      case 3:
+        return 'Jenis Kelamin';
+      case 4:
+        return 'Golongan Darah';
+      case 5:
+        return 'Alamat';
+      case 6:
+        return 'Agama';
+      case 7:
+        return 'Status Perkawinan';
+      case 8:
+        return 'Pekerjaan';
+      case 9:
+        return 'Kewarganegaraan';
+      case 10:
+        return 'Berlaku Hingga';
+      default:
+        return '';
+    }
   }
 }
